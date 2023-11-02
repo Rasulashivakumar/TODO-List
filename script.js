136303fbd7760e9928e4efd1e77dc96ef058773c
Rasulashivakumar <62737436+Rasulashivakumar@users.noreply.github.com>
2023-11-02 19:27:45 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -7,17 +7,16 @@ addTaskButton.addEventListener("click", function () {
     const taskText = taskInput.value.trim();
 
     if (taskText !== "") {
-        // Create a new list item
         const listItem = document.createElement("li");
 
-        // Create a checkbox and a delete button
+        
         const checkbox = document.createElement("input");
         checkbox.type = "checkbox";
         const deleteButton = document.createElement("button");
         deleteButton.classList.add("delete");
         deleteButton.textContent = "Delete";
 
-        // Add event listeners for checkbox and delete button
+        // Adding event listeners for checkbox and delete button
         checkbox.addEventListener("change", function () {
             if (checkbox.checked) {
                 listItem.classList.add("completed");
@@ -31,18 +30,18 @@ addTaskButton.addEventListener("click", function () {
             updateTaskCount();
         });
 
-        // Add the task text, checkbox, and delete button to the list item
+        // For Adding the task text, checkbox, and delete button to the list item
         listItem.textContent = taskText;
         listItem.appendChild(checkbox);
         listItem.appendChild(deleteButton);
 
-        // Add the list item to the task list
+        // To Add the list item to the task list
         taskList.appendChild(listItem);
 
-        // Clear the input field
+        // To Clear the input field
         taskInput.value = "";
 
-        // Update the task count
+        // To Update the task count
         updateTaskCount();
     }
 });
